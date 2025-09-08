6d4babaf293623d554982df962863a0ea9bcd8a1
Marcus Byhring Ølnes <mbolnes@gmail.com>
2025-09-08 16:15:49 +0200

    Level1.tscn and Level2.tscn swap
    Changed the order of levels, after user testing and feedback

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,15 +1,13 @@
 extends Node2D
 
-@onready var SceneBytte = $SceneFader/AnimationPlayer
 
 var level := 1
 var neste_level_tall := level + 1
 var neste_level_navn := "res://scener/level%d.tscn" % neste_level_tall
 
-func _on_portal_body_entered(body: Node2D)-> void:
+
+func _on_portal_body_entered(body: Node2D) -> void:
 	if body is Player:
-		pass
-		
+		print("spiller i kollisjonssonen")
 		GameManager.tot_coins+=GameManager.coins
-		GameManager.coins=0
 		get_tree().change_scene_to_file(neste_level_navn)
