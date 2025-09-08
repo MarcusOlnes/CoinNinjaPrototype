0ae36880a98db5fbe9b0f99d5855747028cc03b2
Marcus Byhring Ølnes <mbolnes@gmail.com>
2025-09-08 16:54:03 +0200

    level5.tscn completion
    level 6 started aswell.

--- scripts/level_5.gd
+++ scripts/level_5.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
+var level := 5
+var neste_level_tall := level + 1
+var neste_level_navn := "res://scener/level%d.tscn" % neste_level_tall
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_portal_body_entered(body: Node2D) -> void:
+	if body is Player:
+		print("spiller i kollisjonssonen")
+		GameManager.tot_coins+=GameManager.coins
+		get_tree().change_scene_to_file(neste_level_navn)
