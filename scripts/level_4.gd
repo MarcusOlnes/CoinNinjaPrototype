ef65b9a5b149554fa550757db930a563652b0a7f
Marcus Byhring Ølnes <mbolnes@gmail.com>
2025-09-08 11:03:04 +0200

    Level4.tscn finished
    Finished editing of level4.tscn, with portal to level5

---
+++ scripts/level_4.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+
+var level := 4
+var neste_level_tall := level + 1
+var neste_level_navn := "res://scener/level%d.tscn" % neste_level_tall
+
+
+func _on_portal_body_entered(body: Node2D) -> void:
+	if body is Player:
+		print("spiller i kollisjonssonen")
+		GameManager.tot_coins+=GameManager.coins
+		get_tree().change_scene_to_file(neste_level_navn)
