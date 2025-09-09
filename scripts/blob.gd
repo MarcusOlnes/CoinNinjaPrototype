336ceedefcdb9a6f22e2f2589f6fc197fee0b3be
Marcus Byhring Ølnes <mbolnes@gmail.com>
2025-09-09 09:21:04 +0200

    level7.tscn finished
    creation of level7.tscn with portal

--- scripts/blob.gd
+++ scripts/blob.gd
@@ -13,6 +13,8 @@ var direction=1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	
 	if ray_cast_hoyre.is_colliding():
 		direction=-1
 		animated_sprite_2d.flip_h=true
