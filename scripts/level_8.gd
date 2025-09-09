55333355a4420da24e68d699110f74dd78f1cb34
Marcus Byhring Ølnes <mbolnes@gmail.com>
2025-09-09 16:17:04 +0200

    edit of level9.tscn
    Beginning of designing level9.tscn design

--- scripts/level_8.gd
+++ scripts/level_8.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
-
 var level := 8
 var neste_level_tall := level + 1
 var neste_level_navn := "res://scener/level%d.tscn" % neste_level_tall
-
-
 
 func _on_portal_body_entered(body: Node2D) -> void:
 	if body is Player:
