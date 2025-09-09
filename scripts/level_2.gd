7cd57ed1ba3f4656833100fb74c89021ec2ce1ba
Marcus Byhring Ølnes <mbolnes@gmail.com>
2025-09-10 00:27:36 +0200

    Scripts update
    changed code, added a line everywhere so that the coins-counter resets every level, but saves the total coins in a new GameManager variable called tot_coins

--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -8,8 +8,6 @@ var neste_level_navn := "res://scener/level%d.tscn" % neste_level_tall
 
 func _on_portal_body_entered(body: Node2D)-> void:
 	if body is Player:
-		pass
-		
 		GameManager.tot_coins+=GameManager.coins
 		GameManager.coins=0
 		get_tree().change_scene_to_file(neste_level_navn)
--- scripts/level_5.gd
+++ scripts/level_5.gd
@@ -9,4 +9,5 @@ func _on_portal_body_entered(body: Node2D) -> void:
 	if body is Player:
 		print("spiller i kollisjonssonen")
 		GameManager.tot_coins+=GameManager.coins
+		GameManager.coins=0
 		get_tree().change_scene_to_file(neste_level_navn)
