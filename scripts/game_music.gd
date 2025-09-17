02710cb3e06f10ac92d69bb1c95f1629e1f802ee
Marcus Byhring Ølnes <mbolnes@gmail.com>
2025-09-17 17:17:07 +0200

    Updated web-export

--- scripts/game_music.gd
+++ scripts/game_music.gd
@@ -1,9 +1,10 @@
 extends AudioStreamPlayer
 
+	
 func _ready():
 	autoplay = false
 	stream_paused = false
-	bus = "Music"
+	bus = "Master"
 
 	# Viktig: gjør at musikken fortsetter selv om spillet er pauset
 	process_mode = Node.PROCESS_MODE_ALWAYS
